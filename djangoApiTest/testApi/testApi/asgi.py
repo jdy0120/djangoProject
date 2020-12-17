248b9b0ea22d8ca126911b01eb34a47e37895b33
jdy0120 <jdy0210@naver.com>
2020-12-17 23:23:12 +0900

    restapi

--- djangoApiTest/testApi/testApi/asgi.py
+++ djangoApiTest/testApi/testApi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for testApi project.
+ASGI config for testapi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testApi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapi.settings')
 
 application = get_asgi_application()
