19cddef134c8ca04607a84dad043c91f16e0a5d4
jdy0120 <jdy0210@naver.com>
2020-12-24 11:03:09 +0900

    button and cart update

--- djangoWebsite/ecommerce/store/views.py
+++ djangoWebsite/ecommerce/store/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+
 from . models import *
 
 # Create your views here.
 
 def store(request): #메인
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items'] # store 페이지에서 장바구니에 몇개 있는지 표시하기 위해 사용
+
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products, 'cartItems':cartItems}
     return render(request, 'store/store.html',context)
 
 def cart(request): # 카트
@@ -14,11 +27,13 @@ def cart(request): # 카트
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/cart.html',context)
 
 
@@ -27,9 +42,38 @@ def checkout(request): # 결제
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/checkout.html',context)
+
+
+def updateItem(request): # 상품의 '장바구니에 넣기'를 클릭하면 카트에서 숫자가 증가 또는 생김
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('productId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
